6daf862d0f42d515570d6a3be978159b5cd40fce
John Lyons <jwlyons2015@gmail.com>
2026-03-11 21:38:39 -0400

    Fetching and Updating Data with React query 28 Exercise -- Removing Duplciate Interfaces

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,8 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
-import apiClient from "../services/api-client";
-import { FetchResponse } from "../services/api-client";
+import APIClient, {FetchResponse } from "../services/api-client";
 import { Platform } from "./usePlatforms";
+
+const apiClient = new APIClient<Game>('/games');
 
 
 export interface Game {
@@ -20,15 +21,15 @@ const useGames = (gameQuery: GameQuery) =>
     // anytime these values change react query update backend
     queryKey: ['games', gameQuery],
     queryFn: () => 
-      apiClient.get<FetchResponse<Game>>('/games', {
-      params: {
+      apiClient
+        .getAll({
+              params: {
         genres: gameQuery.genre?.id,
         parent_platforms: gameQuery.platform?.id,
         ordering: gameQuery.sortOrder,
         search: gameQuery.searchText
       },
-      })
-      .then(res => res.data)
+    }),
   })
 
 
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 
 
 export interface FetchResponse<T> {
@@ -7,9 +7,26 @@ export interface FetchResponse<T> {
 }
 
 
-export default axios.create({
+const axiosInstance = axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
     key: "3a28329946c34b9e9b3d7c09484ef02a",
   },
 });
+
+class APIClient<T> {
+  endpoint: string;
+
+  constructor(endpoint: string) {
+  this.endpoint = endpoint;
+  }
+
+  getAll(config: AxiosRequestConfig) {
+    return axiosInstance 
+      .get<FetchResponse<T>>(this.endpoint)
+      .then(res => res.data)
+  }
+
+} 
+
+export default APIClient;
